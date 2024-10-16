4513e6d328939eaab1a25f5a3122f60315d9f157
KTAGirl <ktagirl24@hotmail.com>
2024-10-16 20:14:13 +0200

    removed terminating entry from master.json

--- wj2git.py
+++ wj2git.py
@@ -4,6 +4,8 @@ import gzip
 import traceback
 import json
 
+DBG = True
+
 def traceReader(x):
     # print("'"+str(x)+"'")
     pass
@@ -341,13 +343,17 @@ files.sort()
 nwarn = 0
 with open('..\\GitHub\\master.json','wt',encoding="utf-8") as wfile:
     wfile.write('[\n')
+    nf = 0
     for fpath in files:
         archiveEntry, archive = findFile(chc,archives,archiveEntries,fpath)
         idx = fpath.find('\\MO2\\')
         assert(idx>=0)
         fpath='..'+fpath[idx:]
+        if nf:
+            wfile.write(",\n")
+        nf += 1
         if archiveEntry == None:
-            wfile.write( '{ "path":'+escapeJSON(fpath)+', "warning":"NOT FOUND IN ARCHIVES" },\n');
+            wfile.write( '{ "path":'+escapeJSON(fpath)+', "warning":"NOT FOUND IN ARCHIVES" }');
             nwarn += 1
         else:
             wfile.write( '{ "path":'+escapeJSON(fpath)+', "hash":"'+str(archiveEntry.file_hash)+'", "size":"'+str(archiveEntry.file_hash)+'", "archive_hash":"'+str(archiveEntry.archive_hash)+'", "in_archive_path":[')
@@ -357,14 +363,14 @@ with open('..\\GitHub\\master.json','wt',encoding="utf-8") as wfile:
                     wfile.write(',')
                 wfile.write(escapeJSON(path))
                 np += 1
-            wfile.write('] },\n')
+            wfile.write('] }')
             # print(archiveEntry.__dict__)
             # print(archive.__dict__)
-    wfile.write('{ "path":""}\n]\n')
+    wfile.write('\n]\n')
             
 print("nn="+str(nn)+" nwarn="+str(nwarn))
 
 #validating json
-if True:
+if DBG:
     with open('..\\GitHub\\master.json', 'rt',encoding="utf-8") as rfile:
         dummy = json.load(rfile)
