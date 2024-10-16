b02a74103ac97b316f6b3c161cffb1c1c6c05aa3
KTAGirl <ktagirl24@hotmail.com>
2024-10-16 14:21:57 +0200

    findFile() (with resolution etc.)

--- wj2git.py
+++ wj2git.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 import gzip
+import traceback
 
 def traceReader(x):
     # print("'"+str(x)+"'")
@@ -15,7 +16,7 @@ class BinaryReader:
         len = int(self.contents[self.offset])
         self.offset += 1
         # print(len)
-        bytes = contents[self.offset:self.offset+len]
+        bytes = self.contents[self.offset:self.offset+len]
         self.offset += len
         s = bytes.decode('cp1252')
         traceReader(s)
@@ -102,7 +103,7 @@ class Img:
 class HashedFile:
     def __init__(self,br):
         traceReader('HashedFile:')
-        self.name = br.ReadString()
+        self.path = br.ReadString()
         self.hash = br.ReadUint64()
         if br.ReadBoolean():
             self.img = Img(br)
@@ -119,7 +120,7 @@ class HashedFile:
             self.children.append(HashedFile(br))
             
     def dbg(self):
-        s = '{ name='+self.name+' hash='+str(self.hash)
+        s = '{ path='+self.path+' hash='+str(self.hash)
         if self.img:
             s += ' img=' + self.img.dbg()
         if len(self.children):
@@ -134,33 +135,150 @@ class HashedFile:
         s += ' }'
         return s
 
-home_dir = os.path.expanduser("~")
-con = sqlite3.connect(home_dir+'/AppData/Local/Wabbajack/GlobalVFSCache5.sqlite')
-cur = con.cursor()
-rowi = -1
-nn = 0
-nx = 0
-for row in cur.execute('SELECT Hash,Contents FROM VFSCache'): # WHERE Hash=-8778729428874073019"):
-    rowi += 1
-    # print(row)
-    # print(row[0])
-    contents = row[1]
-    # print(contents)
-    # with open('gzipped', 'wb') as wfile:
-    #    wfile.write(contents)
+def parseContents(hash,contents):
     contents = gzip.decompress(contents)
     # print(contents)
     # print(rowi)
     # print(contents)
     try:
-        nn += 1
         br = BinaryReader(contents)
         
         hf = HashedFile(br)
         assert(br.isEOF())
         # print(br.contents[br.offset:])
-        print(str(row[0])+':'+hf.dbg())
+        # print(str(hash)+':'+hf.dbg())
+        return hf
     except Exception as e:
         print(e)
-        nx += 1
-print('nn='+str(nn)+' nx='+str(nx))
+        print(traceback.format_exc())
+        return None
+
+def normalizeHash(hash):
+    if hash < 0:
+        return hash + (1<<64)
+    else:
+        return hash
+
+class ArchiveEntry:
+    def __init__(self,archive_hash,intra_path,file_size,file_hash):
+        self.archive_hash = archive_hash
+        self.intra_path = intra_path
+        self.file_size = file_size
+        self.file_hash = file_hash
+
+def loadVFS():
+    home_dir = os.path.expanduser("~")
+    con = sqlite3.connect(home_dir+'/AppData/Local/Wabbajack/GlobalVFSCache5.sqlite')
+    cur = con.cursor()
+    # rowi = -1
+    nn = 0
+    nx = 0
+    archiveEntries = {}
+    for row in cur.execute('SELECT Hash,Contents FROM VFSCache'): # WHERE Hash=-8778729428874073019"):
+        # rowi += 1
+        # print(row)
+        # print(row[0])
+        contents = row[1]
+        # print(contents)
+        # with open('gzipped', 'wb') as wfile:
+        #    wfile.write(contents)
+        hf = parseContents(row[0],contents)
+        nn += 1
+        if hf == None:
+            nx += 1
+        else:    
+            for child in hf.children:
+                if len(child.children)>0:
+                    print("TODO: nested children: path="+child.path)
+                    print(child.dbg())
+                if archiveEntries.get(child.hash)!=None:
+                    print("TODO: multiple entries for a file")
+                archiveEntries[child.hash] = ArchiveEntry(hf.hash,child.path,child.size,child.hash)
+    print('loadVFS: nn='+str(nn)+' nx='+str(nx))
+    return archiveEntries
+
+class Archive:
+    def __init__(self,archive_hash,archive_modified,archive_path):
+        self.archive_hash=archive_hash
+        self.archive_modified=archive_modified
+        self.archive_path=archive_path
+        
+    def eq(self,other):
+        if self.archive_hash != other.archive_hash:
+            return False
+        if self.archive_modified != other.archive_modified:
+            return False
+        if self.archive_path != other.archive_path:
+            return False
+        return True
+
+def loadHC():
+    home_dir = os.path.expanduser("~")
+    con = sqlite3.connect(home_dir+'/AppData/Local/Wabbajack/GlobalHashCache2.sqlite')
+    cur = con.cursor()
+    archives = {}
+    nn = 0
+    for row in cur.execute('SELECT Path,LastModified,Hash FROM HashCache'):
+        hash = normalizeHash(row[2])
+        
+        olda = archives.get(hash)
+        newa = Archive(hash,row[1],row[0])
+        if olda!=None and not olda.eq(newa):
+            # print("TODO: multiple archives: hash="+str(hash)+" old="+str(olda.__dict__)+" new="+str(newa.__dict__))
+            # wait = input("Press Enter to continue.")
+            pass
+        else:
+            archives[hash] = newa
+    print('loadHC: nn='+str(nn))
+    return archives
+    
+def findFile(chc,archives,archiveEntries,fpath):
+    chc.execute("SELECT Path,LastModified,Hash FROM HashCache WHERE Path='"+fpath.lower()+"'")
+    row = chc.fetchone()
+    print(row)
+
+    hash=normalizeHash(row[2])
+    archiveEntry = archiveEntries[hash]
+    #print(archiveEntry.__dict__)
+
+    ahash = archiveEntry.archive_hash
+    archive = archives[ahash]
+    #print(archive.__dict__)
+    return archiveEntry, archive
+
+
+#############
+
+archives = loadHC()
+archiveEntries = loadVFS()
+
+fpath = "C:\\Modding\\MO2\\mods\\Hvergelmir's Aesthetics - Brows\\Brows.esp"
+fpath = fpath.replace("'","''")
+
+home_dir = os.path.expanduser("~")
+hc = sqlite3.connect(home_dir+'/AppData/Local/Wabbajack/GlobalHashCache2.sqlite')
+#vfsc = sqlite3.connect(home_dir+'/AppData/Local/Wabbajack/GlobalVFSCache5.sqlite')
+chc = hc.cursor()
+#cvfsc = vfsc.cursor()
+
+archiveEntry, archive = findFile(chc,archives,archiveEntries,fpath)
+print(archiveEntry.__dict__)
+print(archive.__dict__)
+
+#chc.execute("SELECT Path,LastModified,Hash FROM HashCache WHERE Path='"+fpath.lower()+"'")
+#row = chc.fetchone()
+#print(row)
+
+#hash=normalizeHash(row[2])
+#archiveEntry = archiveEntries[hash]
+#print(archiveEntry.__dict__)
+
+#ahash = archiveEntry.archive_hash
+#archive = archives[ahash]
+#print(archive.__dict__)
+
+#cvfsc.execute("SELECT Hash,Contents FROM VFSCache WHERE Hash="+str(hash))
+#row2 = cvfsc.fetchone()
+#print(row2)
+#hf = parseContents(row2[1])
+#hf.dbg()
