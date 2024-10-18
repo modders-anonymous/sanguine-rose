52cf62d86485fd4a8f18436a8c9f728732224876
KTAGirl <ktagirl24@hotmail.com>
2024-10-18 12:45:36 +0200

    ownmods from source, warnings down to 3527

--- wj2git.py
+++ wj2git.py
@@ -128,7 +128,7 @@ def aEntries(paths,hf,root_archive_hash):
             #print('NESTED:')
             #for ae in aes:
             #    print(ae.__dict__)
-            #dbg.dbgWait();
+            #dbg.dbgWait()
     return aes
 
 def loadVFS():
@@ -527,11 +527,9 @@ def wj2git(mo2,compiler_settings_fname,targetgithub,config,stats):
     with open(targetgithub+'master.json','wt',encoding="utf-8") as wfile:
         wfile.write('[\n')
         nf = 0
-        for fpath in files:
-            archiveEntry, archive = findFile(chc,archives,archiveEntries,fpath)
-            
-            assert(fpath.lower().startswith(mo2abs.lower()))
-            fpath = fpath[len(mo2abs):]
+        for fpath0 in files:            
+            assert(fpath0.lower().startswith(mo2abs.lower()))
+            fpath = fpath0[len(mo2abs):]
             toignore=False
             for ign in ignore:
                 if fpath.startswith(ign):
@@ -543,6 +541,23 @@ def wj2git(mo2,compiler_settings_fname,targetgithub,config,stats):
             if nf:
                 wfile.write(",\n")
             nf += 1
+            
+            isown = False
+            # print(fpath)
+            for own in config['ownmods']:
+                ownpath = 'mods\\'+own+'\\'
+                # print(ownpath)
+                # print(fpath)
+                if fpath.startswith(ownpath):
+                    isown = True
+                    break
+            if isown:
+                targetpath0 = targetdir + fpath
+                wfile.write( '    { "path":'+escapeJSON(fpath)+', "source":'+escapeJSON(targetpath0)+' }')
+                # dbg.dbgWait()
+                continue
+            
+            archiveEntry, archive = findFile(chc,archives,archiveEntries,fpath0)
             if archiveEntry == None:
                 processed = False
                 m = re.search('^mods\\\\(.*)\\\\meta.ini$',fpath)
@@ -558,10 +573,10 @@ def wj2git(mo2,compiler_settings_fname,targetgithub,config,stats):
                         shutil.copyfile(srcpath,targetpath)
                         processed = True
                         # dbg.dbgWait()
-                        wfile.write( '    { "path":'+escapeJSON(fpath)+', "source":'+escapeJSON(targetpath0)+' }');
+                        wfile.write( '    { "path":'+escapeJSON(fpath)+', "source":'+escapeJSON(targetpath0)+' }')
                                 
                 if not processed:
-                    wfile.write( '    { "path":'+escapeJSON(fpath)+', "warning":"NOT FOUND IN ARCHIVES" }');
+                    wfile.write( '    { "path":'+escapeJSON(fpath)+', "warning":"NOT FOUND IN ARCHIVES" }')
                     nwarn += 1
             else:
                 wfile.write( '    { "path":'+escapeJSON(fpath)+', "hash":"'+str(archiveEntry.file_hash)+'", "size":"'+str(archiveEntry.file_hash)+'", "archive_hash":"'+str(archiveEntry.archive_hash)+'", "in_archive_path":[')
