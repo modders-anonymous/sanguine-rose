9c8619ce6c54bc9b03143a123f03d3c13056afa8
KTAGirl <ktagirl24@hotmail.com>
2024-12-14 21:26:20 +0100

    Still WIP: choose_retrievers() (still without greedy part)

--- sanguine/available_files.py
+++ sanguine/available_files.py
@@ -78,6 +78,14 @@ class AvailableFiles:
     def ready_task_name() -> str:
         return AvailableFiles._READYOWNTASKNAME
 
+    def file_retrievers_by_hash(self, h: bytes) -> list[FileRetriever]:
+        zero = ZeroFileRetriever.make_retriever_if(self, h)
+        if zero is not None:
+            return [zero]  # if it is zero file, we won't even try looking elsewhere
+        archived = self._archived_file_retrievers_by_hash(h)
+        github = self._github_file_retrievers_by_hash(h)
+        return archived + github
+
     ### lists of file retrievers
     def _single_archive_retrievers(self, h: bytes) -> list[FileRetrieverFromSingleArchive]:
         found = self._master_data.archived_file_by_hash(h)
@@ -149,17 +157,7 @@ class AvailableFiles:
         self._add_nested_to_retrievers(out, singles)
         assert len(out) > 0
         return out
-    '''
 
-    def file_retrievers_by_hash(self, h: bytes) -> list[FileRetriever]:
-        zero = ZeroFileRetriever.make_retriever_if(self, h)
-        if zero is not None:
-            return [zero]  # if it is zero file, we won't even try looking elsewhere
-        archived = self._archived_file_retrievers_by_hash(h)
-        github = self._github_file_retrievers_by_hash(h)
-        return archived + github
-
-    '''
     def file_retrievers_by_name(self, fname: str) -> list[FileRetriever]:
         # TODO: add retrievers from ownmods
         return self._archived_file_retrievers_by_name(fname)
@@ -220,6 +218,215 @@ class AvailableFiles:
         for f in self._github_cache.all_files():
             add_to_dict_of_lists(self._github_cache_by_hash, f.file_hash, f)
         self._is_ready = True
+
+
+### choose_retrievers()
+
+def _archive_hash(r: FileRetriever) -> bytes | None:
+    if isinstance(r, FileRetrieverFromSingleArchive):
+        return r.archive_hash
+    if isinstance(r, FileRetrieverFromNestedArchives):
+        return r.single_archive_retrievers[0].archive_hash
+    return None
+
+
+def _filter_with_used(inlist: list[tuple[bytes, list[FileRetriever]]], out: list[tuple[bytes, FileRetriever | None]],
+                      used_archives: dict[bytes, int]) -> list[tuple[bytes, list[FileRetriever]]]:
+    filtered: list[tuple[bytes, list[FileRetriever]]] = []
+    for x in inlist:
+        (h, retrs) = x
+        assert len(retrs) >= 2
+        done = False
+        for r in retrs:
+            arh = _archive_hash(r)
+            assert arh is not None
+            if arh in used_archives:
+                out.append((h, r))
+                used_archives[arh] += 1
+                done = True
+                break  # for r
+
+        if not done:
+            filtered.append((h, retrs))
+
+    return filtered
+
+
+def _separate_cluster_step(inlist: list[tuple[bytes, list[FileRetriever]]],
+                           cluster: list[tuple[bytes, list[FileRetriever]]],
+                           cluster_archives: dict[bytes, int]) -> list[tuple[bytes, list[FileRetriever]]]:
+    filtered: list[tuple[bytes, list[FileRetriever]]] = []
+    for x in inlist:  # this code is close, but not identical to the one in _filter_with_used()
+        (h, retrs) = x
+        assert len(retrs) >= 2
+        found = False
+        for r in retrs:
+            arh = _archive_hash(r)
+            assert arh is not None
+            if arh in cluster_archives:
+                cluster_archives[arh] += 1
+            else:
+                cluster_archives[arh] = 1
+            found = True
+            # no break here
+
+        if found:
+            cluster.append((h, retrs))
+        else:
+            filtered.append((h, retrs))
+    return filtered
+
+
+def _separate_cluster(inlist: list[tuple[bytes, list[FileRetriever]]],
+                      cluster: list[tuple[bytes, list[FileRetriever]]],
+                      cluster_archives: dict[bytes, int]) -> list[tuple[bytes, list[FileRetriever]]]:
+    prev = inlist
+    while True:
+        oldclusterlen = len(cluster)
+        filtered: list[tuple[bytes, list[FileRetriever]]] = _separate_cluster_step(prev, cluster, cluster_archives)
+        assert len(filtered) <= len(prev)
+        assert len(prev) - len(filtered) == len(cluster) - oldclusterlen
+        if len(filtered) < len(prev):
+            prev = filtered
+            continue
+
+        return prev
+
+
+_MAX_EXPONENT_RETRIEVERS = 20  # 2**20 is a million, within reason
+
+
+def _make_masked_set(cluster_archives: list[bytes], mask: int) -> dict[bytes, int]:
+    filtered_archives = {}
+    for i in range(len(cluster_archives)):
+        if ((1 << i) & mask) != 0:
+            h = cluster_archives[i]
+            filtered_archives[h] = 1
+    return filtered_archives
+
+
+def _covers_set(cluster: list[tuple[bytes, list[FileRetriever]]], filtered_archives: dict[bytes, int]) -> bool:
+    for x in cluster:
+        (h, retrs) = x
+        for r in retrs:
+            arh = _archive_hash(r)
+            assert arh is not None
+            if arh not in filtered_archives:
+                return False
+    return True
+
+
+def _cost_of_set(filtered_archives: dict[bytes, int], archive_weights: dict[bytes, float]) -> float:
+    out = 0.
+    for arh in filtered_archives:
+        out += archive_weights[arh]
+    return out
+
+
+def _full_search_retrievers(out: list[tuple[bytes, FileRetriever | None]],
+                            cluster: list[tuple[bytes, list[FileRetriever]]], cluster_archives0: dict[bytes, int],
+                            archive_weights: dict[bytes, float]):
+    cluster_archives = [h for h in cluster_archives0.keys()]
+    assert len(cluster_archives) <= _MAX_EXPONENT_RETRIEVERS
+    bestcost = None
+    bestset = None
+    for mask in range(2 ** len(cluster_archives)):
+        filtered_archives = _make_masked_set(cluster_archives, mask)
+        if _covers_set(cluster, filtered_archives):
+            cost = _cost_of_set(filtered_archives, archive_weights)
+            if bestcost is None or cost < bestcost:
+                bestcost = cost
+                bestset = filtered_archives
+
+    assert bestset is not None
+    for x in cluster:
+        (h, retrs) = x
+        done = False
+        for r in retrs:
+            arh = _archive_hash(r)
+            assert arh is not None
+            if arh in bestset:
+                out.append((h, r))
+                done = True
+                break  # for x
+
+        assert done
+
+
+def choose_retrievers(inlist: list[tuple[bytes, list[FileRetriever]]], archive_weights: dict[bytes, float]) -> list[
+    tuple[bytes, FileRetriever | None]]:
+    out: list[tuple[bytes, FileRetriever | None]] = []
+
+    # first pass: choosing unique ones, as well as GitHub ones
+    remaining: list[tuple[bytes, list[FileRetriever]]] = []
+    used_archives: dict[bytes, int] = {}
+    for x in inlist:
+        (h, retrs) = x
+        if len(retrs) == 0:
+            out.append((h, None))
+            continue
+        elif len(retrs) == 1:
+            out.append((h, retrs[0]))
+            arh = _archive_hash(retrs[0])
+            if arh is not None:
+                if arh not in used_archives:
+                    used_archives[arh] = 1
+                else:
+                    used_archives[arh] += 1
+            continue
+
+        done = False
+        for r in retrs:
+            if isinstance(r, GithubFileRetriever):
+                out.append((h, r))
+                done = True
+                break  # for r
+
+        if done:
+            continue  # for x
+
+        # cannot do much now, placing it to remaining[]
+        if __debug__:
+            for r in retrs:
+                assert isinstance(r, FileRetrieverFromSingleArchive) or isinstance(r, FileRetrieverFromNestedArchives)
+        remaining.append((h, retrs))
+
+    # separate into clusters
+    remaining = _filter_with_used(remaining, out, used_archives)
+    clusters: list[list[tuple[bytes, list[FileRetriever]]]] = []
+    clusters_archives: list[dict[bytes, int]] = []
+    while len(remaining) > 0:
+        cluster: list[tuple[bytes, list[FileRetriever]]] = [remaining[0]]
+        remaining = remaining[1:]
+        cluster_archives = {}
+        for x in cluster[0]:
+            (h, retrs) = x
+            arh = _archive_hash(h)
+            assert arh is not None
+            if arh in cluster_archives:
+                cluster_archives[arh] += 1
+            else:
+                cluster_archives[arh] = 1
+
+        oldremaininglen = len(remaining)
+        oldclusterlen = len(cluster)
+        remaining = _separate_cluster(remaining, cluster, cluster_archives)
+        assert len(remaining) <= oldremaininglen
+        assert len(cluster) - oldclusterlen == oldremaininglen - len(remaining)
+        clusters.append(cluster)
+        clusters_archives.append(cluster_archives)
+
+    assert len(clusters_archives) == len(clusters)
+    for i in range(len(clusters)):
+        cluster = clusters[i]
+        cluster_archives = clusters_archives[i]
+        if len(cluster_archives) <= _MAX_EXPONENT_RETRIEVERS:
+            _full_search_retrievers(out, cluster, cluster_archives, archive_weights)
+
+        # "greedy" optimization
+        # TODO!
+
+    return out
 
 
 if __name__ == '__main__':
