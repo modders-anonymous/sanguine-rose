4c94b0f392590278141d2c9daefc17a3434771fe
KTAGirl <ktagirl24@hotmail.com>
2025-02-10 09:17:02 +0100

    Fix for INI patcher (handling deletes)

--- sanguine/plugins/patch/x30ini.py
+++ sanguine/plugins/patch/x30ini.py
@@ -20,16 +20,35 @@ class _IniPatchPluginOverwrite:
         return cls('', '', '')
 
 
+class _IniPatchPluginDel:
+    SANGUINE_JSON: list[tuple[str, str]] = [('section', 'sect'), ('name', 'name')]
+    section: str
+    name: str
+
+    def __init__(self, section: str, name: str):
+        self.section = section
+        self.name = name
+
+    @classmethod
+    def for_sanguine_stable_json_load(cls) -> "_IniPatchPluginDel":
+        return cls('', '')
+
+
 class _IniPluginPatch:
     SANGUINE_JSON: list[tuple[str, str]] = [('overwrites', 'over', _IniPatchPluginOverwrite),
-                                            ]
+                                            ('deletes', 'del', _IniPatchPluginDel)]
     overwrites: list[_IniPatchPluginOverwrite]
+    deletes: list[_IniPatchPluginDel]
 
     def __init__(self):
         self.overwrites = []
+        self.deletes = []
 
     def add_overwrite(self, section: str, name: str, value: str) -> None:
         self.overwrites.append(_IniPatchPluginOverwrite(section, name, value))
+
+    def add_delete(self, section: str, name: str) -> None:
+        self.deletes.append(_IniPatchPluginDel(section, name))
 
 
 class IniPatchPlugin(PatchPluginBase):
@@ -62,8 +81,15 @@ class IniPatchPlugin(PatchPluginBase):
                             override = False
                             nmatch += 1
                 if override:
-                    sec = '__SANGUINE_UNNAMED_SECTION__' if section == configparser.UNNAMED_SECTION else section
+                    sec = '__@#$SANGUINE_UNNAMED_SECTION$#@__' if section == configparser.UNNAMED_SECTION else section
                     out.add_overwrite(sec, name, value)
+
+        for section in srcini.sections():
+            for name, value in srcini[section].items():
+                assert isinstance(name, str)
+                assert isinstance(value, str)
+                if section not in dstini.sections() or name not in dstini[section]:
+                    out.add_delete(section, name)
 
         if nmatch == 0:
             return None
